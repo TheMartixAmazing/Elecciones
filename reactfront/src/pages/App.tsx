c8cfcdd148e097cb3e4f3b9bae98b269a5843520
A1EXF6A <100436091+A1EXF6A@users.noreply.github.com>
2024-10-28 21:19:25 -0500

    adicion de propuestas y cambios en app.tsx para mostrar las propuestas en la pagina

--- reactfront/src/pages/App.tsx
+++ reactfront/src/pages/App.tsx
@@ -2,6 +2,7 @@ import { Routes, Route } from 'react-router-dom'
 import CompShowUsers from '../ShowUsers'
 import NavBar from '../components/navbar/NavBar'
 import Candidatos from '../pages/candidatos/Candidatos'
+import CompShowPropuestas from '../pages/propuestas/Propuestas';
 import './App.css'
 
 function App() {
@@ -13,6 +14,7 @@ function App() {
         <Routes>
           <Route path="/candidatos" element={<Candidatos />} />
           <Route path="/users" element={<CompShowUsers />} />
+          <Route path="/propuestas" element={<CompShowPropuestas />} />
         </Routes>
       </main>
     </>
